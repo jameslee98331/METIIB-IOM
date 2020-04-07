44f92cee490067bb1e48e6c8b55168e7f12f8221
jameslee98331 <jameslee.98331@gmail.com>
2020-04-07 19:09:34 +0800

    Add coursework and class exercises

---
+++ coursework/shopping_basket.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from apyori import apriori
+
+sales_data = pd.read_csv('C://Users//James//Desktop//sales.csv')
+
+records = [([str(sales_data.values[i,j]) for j in range(0, 6)]) for i in range(0, sales_data.shape[0])]
+
+association_rules = apriori(
+                            records,
+                            min_support=0.07,
+                            min_confidence=0.8,
+                            min_lift=3,
+                            min_length=2,
+                            max_length=4
+                            )
+association_results = list(association_rules)
+
+rules = []
+supports = []
+confidences = []
+lifts = []
+
+for item in association_results:
+
+    pair = item[0]
+    items = [x for x in pair]
+    rule = str(list(item.ordered_statistics[0].items_base)) + " -> " + str(list(item.ordered_statistics[0].items_add))
+    support = item.support
+    confidence = item[2][0][2]
+    lift = item[2][0][3]
+
+    print("Rule: " + rule)
+    print("Support: " + str(support))
+    print("Confidence: " + str(confidence))
+    print("Lift: " + str(lift))
+    print("=====================================")
+
+    rules.append(rule)
+    supports.append(support)
+    confidences.append(confidence)
+    lifts.append(lift)
+
+print(rules)
+print(supports)
+print(confidences)
+print(lifts)
+
+apriori_results = pd.DataFrame(list(zip(rules, supports, confidences, lifts)),
+               columns =['Rule', 'Support', 'Confidence', 'Lift'])
+apriori_results.to_csv('C://Users//James//Desktop//apriori_results.csv', encoding='utf-8', index=False)
+
+print(apriori_results)
+
+# plt.bar(rules, lifts)
+# plt.show()
